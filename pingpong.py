cd11afb50c753dedd039a907e3c3b3b045a42edd
YokoMinazawa <83871837+YokoMinazawa@users.noreply.github.com>
2021-05-22 17:01:25 +0300

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -3,7 +3,7 @@ from pygame import*
 class GameSprite(sprite.Sprite):
     def __init__(self,player_image, player_x,player_y,player_speed):
         super().__init__()
-        self.image=transform.scale(image.load(player_image),(17,130))
+        self.image=transform.scale(image.load(player_image),(20,130))
         self.speed=player_speed
         self.rect=self.image.get_rect()
         self.rect.x=player_x
